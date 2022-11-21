6c7d8ee01905d883117fa9e062b757e7220a57db
raupsss <androghanz@gmail.com>
2022-11-21 14:13:13 +0700

    latihan jquery

---
+++ latihan jquery/js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    
+    // Preview Chair
+
+    $(".card").click(function (e) {
+        e.preventDefault();
+        const imgPreview = $("img", this).attr("src");
+        $("#preview").attr("src", imgPreview);
+
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    })
+
+    $(".side").click(function (e) {
+        e.preventDefault();
+        const imgPreview = $("img", this).attr("src");
+        $("#preview").attr("src", imgPreview);
+    })
+
+    // Alert
+
+    $("#formEmail").submit(function (e) {
+        e.preventDefault();
+        const email = $("#inputEmail").val();
+        $("#textEmail").html("Terima Kasih " + "<strong>" + email + "</strong>" + ", Link sudah dapat dilihat di email Anda");
+        $("#alertEmail").css("background-color", "pink");
+        $("#alertEmail").removeClass("d-none");
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+
+    $("#alertEmail").click(function (e) {
+        e.preventDefault();
+        $(this).addClass("d-none");
+    });
+
+    // Form Shipping
+
+    $("#formShipping").submit(function (e) {
+        e.preventDefault();
+        $(this).addClass("d-none");
+        const inputAddress = $("#address").val();
+        const inputFirstName = $("#firstName").val();
+        const inputLastName = $("#lastName").val();
+        $("#message").removeClass("d-none");
+        $("#titleMessage").html("Terima Kasih, " + inputFirstName + " " + inputLastName);
+        $("#mainMessage").html("Paket akan dikirim ke " + inputAddress);
+    });
+});
